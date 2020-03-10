8a9b6b7425eea7fd0578d331a006eae9b6d7db6d
Stuart Shepherd <stuartshepherd@gmail.com>
2020-03-11 08:25:35 +1100

    updated title

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace Prototype
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Prototype example");
+            Console.WriteLine("Prototype design pattern example");
             Console.WriteLine("Prototype is a creational design pattern that allows cloning objects, even complex ones, without coupling to their specific classes.");
             Console.WriteLine();
 
